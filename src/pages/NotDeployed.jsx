9b04d33ac3fa03708b7b3e8ec6f3b48768c9d654
Saikat <saikatsaha.rph@gmail.com>
2024-11-08 09:51:02 +0530

    fixed notdeployed page

--- src/pages/NotDeployed.jsx
+++ src/pages/NotDeployed.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 const NotDeployed = () => {
   return (
     <div className='h-screen flex flex-col items-center'>
-      <p className='my-32 border-2 p-4 rounded-lg'>These Projects ain't deployed yet, visit <a href="https://github.com/Saikat-github/NotDeployed" className='cursor-pointer text-pink-700'>Github</a> for source code</p>
-
-      <div>
-
-      </div>
+      <p className='my-32 border-2 p-4 rounded-lg mx-6'>These Projects ain't deployed yet, visit <a href="https://github.com/Saikat-github/NotDeployed" className='cursor-pointer text-pink-700'>Github</a> for source code</p>
     </div>
   )
 }
